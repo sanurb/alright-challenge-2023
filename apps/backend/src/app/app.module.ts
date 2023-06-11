c79f2a3a84c2a578d186920cdfc4fb48a06594cf
Santiago Urbano <david.urbano@uao.edu.co>
2023-06-11 00:18:25 -0500

    feat(app): add ServeStaticModule to serve static files from public directory feat(app): add public directory to tsconfig.app.json include section feat(app): add public directory to repository and create index.html file

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -9,6 +11,9 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '..', 'public'),
     }),
     AuthModule,
   ],
