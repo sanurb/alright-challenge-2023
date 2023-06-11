0f6f28166b0d2b22e09a9d2fba94c9f3eaace153
Santiago Urbano <david.urbano@uao.edu.co>
2023-06-10 23:58:07 -0500

    feat(backend): add support for versioning and increase json payload limit to 10mb deps(backend): add "@nestjs/serve-static": "^3.0.1" to package.json

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,15 +1,23 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import { setupSwagger } from './config/swagger-setup';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
+  app.use(json({ limit: '10mb' }));
+
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
 
