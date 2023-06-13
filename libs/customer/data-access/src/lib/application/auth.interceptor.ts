947a5bd432fe4b8b55202f88008c11fef5aa9bd9
Santiago Urbano <david.urbano@uao.edu.co>
2023-06-13 00:00:53 -0500

    feat(auth.interceptor.ts): add jwtInterceptor to set Authorization header with jwt token from StorageService

--- libs/customer/data-access/src/lib/application/auth.interceptor.ts
+++ libs/customer/data-access/src/lib/application/auth.interceptor.ts
@@ -1,5 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { StorageService } from '@nx-giant/shared/util';
+import { inject } from '@angular/core';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
+export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const storageService = inject(StorageService);
+  const token = storageService.get<string>('jwt');
+
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+
   return next(req);
 };
